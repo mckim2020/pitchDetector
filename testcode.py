a5aa36ca879ab4e6f179b4654be0acfade93ebe3
mckim2020 <61468369+mckim2020@users.noreply.github.com>
2020-03-03 01:12:46 +0900

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -9,7 +9,10 @@ for i in range(len(timeList)):
 
 
 # set interval size = 10000
-newTimeList, pitchList = pitch(timeList, ampList, 10000)
+# r = 100: f range = 0-499Hz
+# r = 50: f range = 0-1000Hz 
+#880 Hz is the highest pitch in orchestra(instruments)
+newTimeList, pitchList = pitch(timeList, ampList, 10000, 50)
 plt.plot(newTimeList, pitchList)
 plt.xlabel('time')
 plt.ylabel('pitch')
