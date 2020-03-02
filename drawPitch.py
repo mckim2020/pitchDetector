4aa17619bcde268976c36ef8044625e0f18dc31d
mckim2020 <61468369+mckim2020@users.noreply.github.com>
2020-03-02 22:46:13 +0900

    Create drawPitch.py
    Functions needed to analyze pitches from time-amplitude data file(csv file format)

---
+++ drawPitch.py
@@ -0,0 +1,86 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import scipy.fftpack
+
+
+# returns time interval of sampled csv file
+def interval(timeList):
+    return abs(timeList[-1] - timeList[0])/(len(timeList) - 1)
+
+
+# returns freq distribution
+# k indicates how rapid you will scan/analyze(maybe sampling)
+def plotFreq(timeList, ampList, k):
+    # number of sample points
+    n = len(timeList)
+
+    # sample spacing
+    ts = interval(timeList)
+    x = y = np.linspace(0.0, n*ts, n)
+    for i in range(len(y)):
+        y[i] = ampList[i]
+    # apply FFT: Nyquist interval --> [-0.5*fs, 0.5fs]
+    # basically a DFT
+    xf = np.linspace(0, 1/(k*ts), n//k)
+    yf = scipy.fftpack.fft(y)
+
+    # plt.plot(xf, 2 / n * np.abs(yf[:n // 2]))
+    # plt.show()
+    return xf, yf
+
+
+# makes timeList and pitchList
+def createTimeAmpList(fileName):
+    timeList = []
+    ampList = []
+
+    # open file
+    f = open(fileName, 'r')
+
+    # append until end comes
+    count = 0
+    for line in f:
+        a, b = line.split(',')
+        count += 1
+        a = a.strip()
+        b = b.strip()
+        if b == '*':
+            break
+        timeList.append(float(a))
+        ampList.append(float(b))
+    # f.close()
+    return timeList, ampList
+
+
+# return most often frequency
+def maxFreq(x, y):
+    freq = 0
+    max = 0
+
+    for i in range(len(x)):
+        if y[i] > max:
+            freq = x[i]
+            max = y[i]
+
+    return freq
+
+
+# return pitchList
+def pitch(timeList, ampList, n):
+    newTimeList = []
+    pitchList = []
+    chunks = [timeList[x:x+n] for x in range(0, len(timeList), n)]
+    # following chunks may run on your computer but not mine: less noise
+    # chunks = [timeList[x:x + n] for x in range(0, len(timeList) - n-1)]
+    amps = [ampList[x:x+n] for x in range(0, len(ampList), n)]
+    for i in range(len(chunks)):
+        newTimeList.append(chunks[i][len(chunks[i]) // 2])
+        xf, yf = plotFreq(chunks[i], amps[i], 100)
+        pitch = maxFreq(xf, yf)
+        pitchList.append(pitch)
+
+    return newTimeList, pitchList
+
+
+
